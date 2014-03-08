1743ff62ea1cf28797bfcc66de12574eb2a56c01
kser2ox <kser2ox@gmail.com>
2014-03-08 19:04:40 +0200

    v03

--- Codecamp/Codecamp/player.cs
+++ Codecamp/Codecamp/player.cs
@@ -50,7 +50,7 @@ namespace Codecamp
         public Image drawFigure()
         {
             Image figure = new Image();
-            figure.Width = 60;
+            figure.Width = 70;
             figure.Height = 50;
             figure.Margin = new Thickness(10, 10, 1000, 1000);
             return figure;
