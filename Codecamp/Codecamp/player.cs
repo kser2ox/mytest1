2b03e21d83e1e2b5175a1ba3bc8f6de18e963bcc
zusamania <zan6000@inbox.lv>
2014-03-08 20:17:18 +0200

    a

--- Codecamp/Codecamp/player.cs
+++ Codecamp/Codecamp/player.cs
@@ -50,7 +50,7 @@ namespace Codecamp
         public Image drawFigure()
         {
             Image figure = new Image();
-            figure.Width = 8880;
+            figure.Width = 8888;
             figure.Height = 50;
             figure.Margin = new Thickness(10, 10, 1000, 1000);
             return figure;
